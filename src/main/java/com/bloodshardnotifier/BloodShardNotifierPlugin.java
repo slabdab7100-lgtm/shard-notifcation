6fcf0e4bda3d69b1d6b44fc21ea3ad9e7706c200
Craccin <328323526+slabdab7100-lgtm@users.noreply.github.com>
2026-09-17 00:17:37 -0400

    Clean up imports for plugin directory sound handling

--- src/main/java/com/bloodshardnotifier/BloodShardNotifierPlugin.java
+++ src/main/java/com/bloodshardnotifier/BloodShardNotifierPlugin.java
@@ -5,7 +5,6 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.concurrent.ScheduledExecutorService;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
